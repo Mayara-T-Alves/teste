acfac30f2f5c5f5339f8e2086b6180d0f5b68f16
Mayara Alves <1206mkr@gmail.com>
2022-03-26 23:18:00 -0300

    Criação e add arq

--- teste.js
+++ teste.js
@@ -1,3 +1,10 @@
 function hello (){
     console.log('oi')
 }
+
+var linguagem = "Javascript";
+console.log("Aprendendo" + linguagem);
+
+//nome pode ser redeclarada
+var libguagem = "java";
+console.log("Aprendedno, " + linguagem);
